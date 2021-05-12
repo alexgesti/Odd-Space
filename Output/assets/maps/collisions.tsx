33c841c7c120df5d18e4332cd22bb4a596e7df05
DESKTOP-1S1V8CE\alexg <alexgesti001@gmail.com>
2021-05-12 19:14:05 +0200

    tsxupdate

---
+++ Output/assets/maps/collisions.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="collisions" tilewidth="32" tileheight="32" tilecount="3" columns="3">
+ <image source="prop_collsions_v01.png" width="96" height="33"/>
+</tileset>
