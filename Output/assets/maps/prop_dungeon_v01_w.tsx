1c802a057a2926ac4e844bb8db37bd04625e5aca
Osvak <oscarcanales2001@gmail.com>
2021-05-12 17:39:34 +0200

    tmx update

--- Output/assets/maps/prop_dungeon_v01_w.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="prop_dungeon_v01_w" tilewidth="32" tileheight="32" tilecount="240" columns="30">
- <image source="prop_dungeon_v01_w.png" width="960" height="256"/>
-</tileset>
