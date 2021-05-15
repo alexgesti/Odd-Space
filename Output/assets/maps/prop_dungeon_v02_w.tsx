b9b7508157021836e7ed70ed9c682006c13a307c
Denisdrk6 <denisdsuniversidad@hotmail.com>
2021-05-15 18:26:16 +0200

    Dungeon map loading bug solved

--- Output/assets/maps/prop_dungeon_v02_w.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="prop_dungeon_v02_w" tilewidth="64" tileheight="64" tilecount="40" columns="10">
- <image source="prop_dungeon_v02_w.png" width="640" height="256"/>
-</tileset>
